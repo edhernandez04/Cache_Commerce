425fd62283bc94080c08e5e3b3a1ebb2de611d2d
Ed <MCHH.004@gmail.com>
2020-12-01 16:55:51 -0500

    bootstrap + navBar

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import {
   Switch,
   Route,
 } from "react-router-dom"
+import Button from 'react-bootstrap/Button'
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div class='row'>
-        <img class='logo' src='https://lh3.googleusercontent.com/_ss2KrA36gFxGG0mVGjdNpzjMqKI6xd3Tznj26yS_UiCXemn8YGjDhLZYxi_9JNA5S1b68eaPIjnPZAiyJT-CB4wCFSHWEQuc-2VzFqxMWFIRGD5kg-01PjqGaix5JRbAhsh-ElrhzEzSznE4--lIq1REe3uMXkbB6YkGEfze7jGvN1AaYtDlZPI06avestI-GMQyTlMlMiW2zBWHgFNNCBWH7hsN57putT-ANAAL4nQkGvnF85OrSlm5T-g4RXvM_HaGeztUipxeJSOIpxgxmGFuT-kdMhvpqLGxvKVeIaf95UIbh9pYyxAH6K-dqF-SA7k8AogLLdFCIRWGek8PSxXi5YPnObVsegp1UngnEMA7KlJv0AIlOgHowyQkAXABGfVR7CK1HaZ95f1aPf5VHLrcKW5W7skzc8Nbjd4LI2h1E_7dIjYDV8OuWPgjutsRwHOON65Zo7aTH8QrWmm9Xfhxzf07isfcWl7Nf49SyT0uX2UwYU_r2-ZgNeQ9sNFDnLmKrg6tvqu_9stCCU4ZKnUJ3hWIz0R3wgmSvfrxug0nkepBXEOj5aeQFjWyR_zp7RBrjFChUTX6lM8B7w68EhCVdQqhERAnbZsYimJJE9zKDJcgbOnbzBmiG2tg9V9IezW1hSupgYUwa-XQEd7MTPerg3bIvNIq6XOlCfHGVCJfw_zFjs150C70lRP=s689-no?authuser=2' alt='cache'></img>
-        <div>
-          <button>Home</button>
-          <button>Shop</button>
-          <button>Profile</button>
-          <button>Cart</button>
+      <div>
+        <div className='row'>
+          <img className='logo' src='https://lh3.googleusercontent.com/fife/ABSRlIpKX1njNFoxgyd73jIGcppMortGIHG0Faw7ltBH9TGbD4zqdPQezqEMVfzzi8rvI_p9Ht5suPIgJlXzyOIAZa4guM3Mt03A0OjRZ_i6sJ7AUl0-w4aYIFI-ayc4hbt5tEF_j3lxTD77IBj0eJvhcDCorUiJNpg8jIp0hT6gd2ovCng2C1HsB4v5zJodLcedq02zRLqlgD_bxfIENPFnKb1OKVqw_VRVEvp4ivW_dYL3dFniiJ81C9GXDe5O0Bp2p3HSoCxIrg84UXtlhP-hhfui2-mmfcoH4O9te9Rl0C9om990VpzY0uuBGYamBICJeOOp1iWM6020iyPcDI0p7UfpaMrSgUNgUwEUV_bfxgK0_3YntlDNNcJ9hoQyQMM7MrYAcIWDKw18vKhzllJGFzx2LcgaBEa0a8JKe-b_GU-Qo6jhwnPrKBtA4K4JFsN-rAV_Pg9nuQ1xJNPxPLt9uPFJv-vqpbTEek2C2Vk2o3KyNETTjOPwdAKWoXyFAt90kFnegJD1pK079qAzRnRDHBJc1qrazYDFEVlBmkOazrm3zLvSgeRY7Ol-YjQaGz-Np5L-atan9gt1L5gn3oMDIz87vE-Vx_Im3qU2CMZz3uBCyPiJEaI0ISGXdUZSv7VPg8k_txazdqf-FYfuLOMNXDCb-LRNN7NhAwkOdxDJrTE1U2CQu0KJMUZMttMZ0nbadAbSQH9aEio9QruxSRi3CvB1zmzhXMV0gh8J=s689-no?authuser=2' alt='cache technologies' />
         </div>
-        <hr />
+        <div className='row'>
+          <Button variant="light">Home</Button>
+          <Button variant="light">Shop</Button>
+          <Button variant="light">Profile</Button>
+          <Button variant="light">Cart</Button>
+        </div>
       </div>
+      <hr />
+
       <Switch>
         <Route path="/">
           {/* <Home /> */}
